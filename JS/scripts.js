9a6bc08166ad7acee9889308c9adcb557b5a73e7
wiejooo <jakub.wiejak98@gmail.com>
2024-09-23 22:24:34 +0200

    prace zwiazane z animacja sekcji chust

--- JS/scripts.js
+++ JS/scripts.js
@@ -11,3 +11,20 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled');
     }
 });
+
+/**
+ * Animacje sekcji chust
+ */
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
